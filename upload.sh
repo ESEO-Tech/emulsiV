8e1d8668971612c0846e7fd49de780fa09e65ac8
Guillaume Savaton <guillaume.savaton@eseo.fr>
2020-06-18 09:28:54 +0200

    Removed upload.sh from version control

--- upload.sh
+++
@@ -1,6 +0,0 @@
-
-DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-
-rsync --archive --delete --checksum --progress --rsh "ssh -p 2204" \
-    --exclude ".git*" --exclude "upload.sh" --delete-excluded \
-    $DIR/ www-data@tice.sea.eseo.fr:/var/www/riscv/
